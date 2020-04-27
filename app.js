9efb7ce4b52530c3bcaaec762806c61bbcab3351
Godwin254 <godwinbee15@gmail.com>
2020-04-27 18:10:47 +0300

    declare two variables (inputName & button)

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 (function(num){
+
+    //declare variables
+    let inputName = document.querySelector(".input1").nodeValue;
+    let button = document.querySelector(".btn");
+
     //expression
     let squareRoot = sqrt(num);
 })()
