e1ce8e95962485216943f5a404ececc7ddb0096a
Godwin254 <godwinbee15@gmail.com>
2020-04-27 18:16:14 +0300

    added an eventListener to the button

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
     let inputName = document.querySelector(".input1").nodeValue;
     let button = document.querySelector(".btn");
 
+    //add an eventListener to the button
+    button.addEventListener('click', () => {
+        
+        //convert the value of input into a text
+        let name = document.createTextNode(inputName);
+
+        //write the name into the document
+
+        document.write(name);
+    });
     //expression
     let squareRoot = sqrt(num);
 })()
